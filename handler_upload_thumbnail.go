2e8e6392b4d52a6caeb9f33055516412b7f41c5a
StratosKyriazidis <st.kiriazidis@smartupweb.com>
2026-04-25 08:58:10 +0300

    second commit

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +33,73 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse image", err)
+		return
+	}
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	mediaType := header.Header.Get("content-type")
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Unsupported image type", errors.New(""))
+		return
+	}
+
+	buf, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read form file", err)
+		return
+	}
+	metadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to get video metadata", err)
+		return
+	}
+	if metadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized user", err)
+		return
+	}
+	tnail := thumbnail{data: buf, mediaType: mediaType}
+	videoThumbnails[videoID] = tnail
+	parts := strings.Split(mediaType, "/")
+	fileExtension := parts[len(parts)-1]
+	fileName := fmt.Sprintf("%s.%s", videoIDString, fileExtension)
+	assetPath := filepath.Join(cfg.assetsRoot, fileName)
+
+	dst, err := os.Create(assetPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error saving to disk", err)
+		return
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error saving to disk", err)
+		return
+	}
+
+	thumbnailURL := fmt.Sprintf(
+		"http://localhost:%s/%s",
+		cfg.port,
+		dst.Name(),
+	)
+	metadata.ThumbnailURL = &thumbnailURL
+	err = cfg.db.UpdateVideo(metadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error saving to SQLite", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, metadata)
 }
